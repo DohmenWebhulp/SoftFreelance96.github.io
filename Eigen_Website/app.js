2d898ac77b46d687740b4c1c413e2eb4789699e1
Jesse Dohmen <jessedohmen1@gmail.com>
2022-08-08 11:20:19 +0200

    added website

---
+++ Eigen_Website/app.js
@@ -0,0 +1,79 @@
+(function () {
+    [...document.querySelectorAll(".control")].forEach(button => {
+        button.addEventListener("click", function() {
+            document.querySelector(".active-btn").classList.remove("active-btn");
+            this.classList.add("active-btn");
+            document.querySelector(".active").classList.remove("active");
+            document.getElementById(button.dataset.id).classList.add("active");
+        })
+    });
+
+    function backgroundChange(){
+        var navbar = document.querySelector(".navbar");
+
+        var B = document.body; 
+        var D = document.documentElement; 
+        D = (D.clientHeight)? D: B;
+
+        if(D.scrollTop != 0){
+            if(document.body.classList.contains("light-mode")){
+                navbar.style.background = "rgba(255, 255, 255, 1)";
+            }else{
+                navbar.style.background = "rgba(25, 29, 43, 1)";
+            }
+        }else{
+            navbar.style.background = "rgba(255, 255, 255, 0)";
+        }
+
+        return D;
+    }
+
+    document.querySelector(".theme-btn").addEventListener("click", () => {
+        document.body.classList.toggle("light-mode");
+        backgroundChange();
+        // var navbar = document.querySelector(".navbar");
+        // var B = document.body;
+        // var D = document.documentElement; 
+        // D = (D.clientHeight)? D: B;
+        // if(D.scrollTop != 0){
+        //     if(document.body.classList.contains("light-mode")){
+        //         navbar.style.background = "rgba(255, 255, 255, 1)";
+        //     }else{
+        //         navbar.style.background = "rgba(25, 29, 43, 1)";
+        //     }
+        // }else{
+        //     navbar.style.background = "rgba(255,255,255,0)";
+        // }
+    });
+    document.querySelector(".language-btn").addEventListener("click", () => {
+        var activeLanguage = document.querySelectorAll(".active-language");
+        var noDisplay = document.querySelectorAll(".no-display");
+
+        activeLanguage.forEach(active => {
+            active.classList.remove("active-language");
+            active.classList.add("no-display");
+        });
+
+        noDisplay.forEach(notActive => {
+            notActive.classList.add("active-language");
+            notActive.classList.remove("no-display");
+        });
+
+        if(document.querySelector(".language").innerText == "EN"){
+            document.querySelector(".language").innerText = "NL";
+        }else{
+            document.querySelector(".language").innerText = "EN";
+        }
+    });
+    document.querySelectorAll(".to-contact").forEach(button => {
+        button.addEventListener("click", function(){
+            document.querySelector(".active-btn").classList.remove("active-btn");
+            document.querySelector(".contact-button").classList.add("active-btn");
+            document.querySelector(".active").classList.remove("active");
+            document.querySelector(".contact").classList.add("active");
+        })
+    });
+
+
+})
+();
